60b5d056cc558be0711a66715b79942dfb85a0ee
sandityasn10 <45733733+sandityasn10@users.noreply.github.com>
2023-01-26 18:40:08 +0700

    add 2
    pagination, controller, vendor pagination

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Carbon;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Pagination\Paginator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,5 +27,6 @@ class AppServiceProvider extends ServiceProvider
     {
         config(['app.locale' => 'id']);
 	    Carbon::setLocale('id');
+        Paginator::useBootstrap();
     }
 }
