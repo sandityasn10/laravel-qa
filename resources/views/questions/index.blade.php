fa4beca5503bb603fe35536fa0c6e02a175a97b1
sandityasn10 <45733733+sandityasn10@users.noreply.github.com>
2023-01-26 22:24:34 +0700

    add4
    add scss

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -10,16 +10,28 @@
                 <div class="card-body">
                     @foreach ($questions as $question)
                         <div class="media">
+                            <div class="d-flex align-items-start counters">
+                                <div class="vote">
+                                    <strong>{{$question->votes}}</strong> {{Str::plural('vote',$question->votes)}}
+                                </div>
+                                <div class="status {{$question->status}}">
+                                    <strong>{{$question->answers}}</strong> {{Str::plural('answer',$question->answers)}}
+                                </div>
+                                <div class="view">
+                                    {{$question->views . " " . Str::plural('view',$question->views)}}
+                                </div>
+                            </div>
                             <div class="media-body">
                                 <h3 class="mt-0"><a href="{{$question->url}}">{{$question->title}}</a></h3>
                                 <p class="lead">
                                     Asked By
                                     <a href="{{$question->user->url}}">{{$question->user->name}}</a>
-                                    <small class="text-muted">{{$question->created_at}}</small>
+                                    <small class="text-muted">{{$question->created_date}}</small>
                                 </p>
                                 {{Str::limit($question->body,250)}}
                             </div>
                         </div>
+                        <br>
                     @endforeach
                     <div class="mx-auto">
                         {{$questions->links()}}
